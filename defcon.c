fe5fe781db4f797b35239fd676e3d530f86aca98
Patrick Schaaf <github@bof.de>
2012-01-17 11:28:40 +0100

    Refactoring code a bit. There is NO change to the parsing logic.
    - fixes to the TSRMLS_ markup (use _CC when calling, _DC in definitions)
    - config file reading and parsing split into separate functions
    - PHP constant definition code split into add_constant function
    - file/line (and module_number) moved to a 'struct defcon_context'
    - added PR_ERR, PR_WARN, and PR_DBG macros, all taking the context struct
      as an argument and automatically adding file/line information.
    - The PR_DBG macro is normally disabled; set DEBUG_OUTPUT at the
      top of defcon.c to enable its output (which goes directly to stderr)

--- defcon.c
+++ defcon.c
@@ -15,6 +15,9 @@
   +----------------------------------------------------------------------+
 */
 
+// set to 1 to enable copious "stderr" debug output during parse
+#define DEBUG_OUTPUT 0
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -22,30 +25,6 @@
 #include "php.h"
 #include "php_ini.h"
 #include "php_defcon.h"
-
-static const char *dt[] = {
-	"string",	/* 0 */
-	"int",		/* 1 */
-	"long",		/* 2 */
-	"float",	/* 3 */
-	"real",		/* 4 */
-	"double",	/* 5 */
-	"bool",		/* 6 */
-	"boolean",	/* 7 */
-	"logical",	/* 8 */
-	"short",	/* 9 */
-	NULL
-};
-
-
-static short match_type(const char *g TSRMLS_DC) {
-	int i=0;
-	for(; dt[i] != NULL; i++) {
-		if(0 == strcasecmp(dt[i], g))
-		return i;
-	}
-	return -1;
-}
 
 static function_entry defcon_functions[] = {
 	{NULL, NULL, NULL}
@@ -83,197 +62,264 @@ PHP_MINFO_FUNCTION(defcon) {
     php_info_print_table_end();
 }
 
+struct defcon_keyword {
+	char *name;
+};
+
+static struct defcon_keyword keywords[] = {
+/*  0 */	{ "string" },
+/*  1 */	{ "int" },
+/*  2 */	{ "long" },
+/*  3 */	{ "float" },
+/*  4 */	{ "real" },
+/*  5 */	{ "double" },
+/*  6 */	{ "bool" },
+/*  7 */	{ "boolean" },
+/*  8 */	{ "logical" },
+/*  9 */	{ "short" },
+};
+#define NR_KW (sizeof(keywords)/sizeof(keywords[0]))
+
+static short match_keyword(const char *g) {
+	int i;
+	for (i = 0; i < NR_KW; i++) {
+		if (0 == strcasecmp(keywords[i].name, g))
+		return i;
+	}
+	return -1;
+}
+
+
+struct defcon_context {
+	int module_number;
+	char *file;
+	int line;
+};
+
+#define PR_ERR(CTX, FMT, ...) \
+	php_error(E_ERROR, "defcon: %s line %i: " FMT, \
+		 (CTX)->file, (CTX)->line, ## __VA_ARGS__)
+#define PR_WARN(CTX, FMT, ...) \
+	php_error(E_WARNING, "defcon: %s line %i: " FMT, \
+		 (CTX)->file, (CTX)->line, ## __VA_ARGS__)
+#if DEBUG_OUTPUT
+#define PR_DBG(CTX, FMT, ...) \
+	fprintf(stderr, "DEFCON %s:%i " FMT, \
+		(CTX)->file, (CTX)->line, ## __VA_ARGS__)
+#else
+#define PR_DBG(CTX, FMT, ...) do {} while (0)
+#endif
+
+static int add_constant(
+	struct defcon_context *ctx,
+	short KW,
+	char *N,
+	char *V
+TSRMLS_DC) {
+	zend_constant zc;
+
+	switch (KW) {
+	   case 0:
+		zc.value.type = IS_STRING;
+		zc.value.value.str.val = zend_strndup(V, strlen(V));
+		zc.value.value.str.len = strlen(V);
+		break;
+	   case 1:
+	   case 2:
+	   case 9:
+		zc.value.type = IS_LONG;
+		zc.value.value.lval = atol(V);
+		break;
+	   case 3:
+	   case 4:
+	   case 5:
+		zc.value.type = IS_DOUBLE;
+		zc.value.value.dval = atof(V);
+		break;
+	   case 6:
+	   case 7:
+	   case 8:
+		zc.value.type = IS_BOOL;
+		if (0 == strcasecmp(V, "true")) {
+			zc.value.value.lval = 1;
+		} else if (0 == strcasecmp(V, "false")) {
+			zc.value.value.lval = 0;
+		} else {
+			zc.value.value.lval = atol(V);
+		}
+		break;
+	}
+	zc.flags = CONST_CS | CONST_PERSISTENT;
+	zc.name = zend_strndup(N, strlen(N));
+	zc.name_len = strlen(N)+1;
+	zc.module_number = ctx->module_number;
+
+	if (zend_register_constant(&zc TSRMLS_CC) == FAILURE)
+		PR_ERR(ctx, "Constant '%s' redefined", N);
+
+	return 1;
+}
+
+static int config_read(struct defcon_context *ctx TSRMLS_DC);
+
+static int config_parse(struct defcon_context *ctx, char *s TSRMLS_DC) {
+	char kw[KEYWORDLEN + 1], N[NAMELEN + 1], V[VALUELEN + 1];
+	int i;
+	char c;
+	short KW, state = 0;
+
+	for(; *s; s++) {
+
+		for(; *s && (*s == ' ' || *s == '\n' || *s == '\t' || *s == '\r'); s++) {
+			if(*s == '\n') ctx->line++;
+		}
+
+		if(!*s)
+		   break;
+
+		if(*s == '#') {
+			for(; *s && *s != '\n'; s++);
+		} else {
+
+			if(*s == ',')
+			  state = 4;
+			else if(*s == ';')
+			  state = 5;
+
+			switch(state) {
+			   case 0: // catch keyword
+				for(i = 0; *s && (*s >= 'a' && *s <= 'z' || *s >= 'A' && *s <= 'Z'); s++, i++) {
+					if(i > KEYWORDLEN) {
+						PR_ERR(ctx, "Typ too long");
+						return 0;
+					}
+					kw[i] = *s;
+				}
+				kw[i] = '\0';
+				KW = match_keyword(kw);
+				if(-1 == KW) {
+					PR_ERR(ctx, "No valid KW (%s)", kw);
+					return 0;
+				}
+				state = 1;
+				break;
+			   case 1: // catch varname
+				for(i = 0; *s >= 'a' && *s <= 'z' || *s >= 'A' && *s <= 'Z' || *s >= '0' && *s <= '9' || *s == '_'; s++, i++) {
+					if(i > NAMELEN) {
+						PR_ERR(ctx, "Varname too long");
+						return 0;
+					}
+					N[i] = *s;
+				}
+				N[i] = '\0';
+				if(N[0] == '\0') {
+					PR_ERR(ctx, "No Varname set");
+					return 0;
+				}
+
+				if(-1 != match_keyword(V)) {
+					PR_ERR(ctx, "Varname should not be a keyword");
+					return 0;
+				}
+
+				state = 2;
+				s--;
+				break;
+				case 2: // catch =
+				if(*s != '=') {
+					PR_ERR(ctx, "Strange input ( = required)", *s);
+					return 0;
+				}
+				state = 3;
+				break;
+			   case 3: // final state
+				if(*s == '"' || *s == '\'') c = *(s++);
+				else c = '\0';;
+
+				for(i=0; *s && (c && *s != c || (!c && (*s >= '0' && *s<='9' || *s >= 'a' && *s<='z' || *s >= 'A' && *s<='Z' || *s == '.'))); s++, i++) {
+					if(i > VALUELEN) {
+						PR_ERR(ctx, "Value too long");
+						return 0;
+					}
+					V[i] = *s;
+				}
+
+				V[i] = '\0';
+
+				if(c) s++;
+				else if(V[0] == '\0') {
+					PR_ERR(ctx, "No Value found");
+					return 0;
+
+				}
+				if (!add_constant(ctx, KW, N, V TSRMLS_CC))
+					return 0;
+
+				state = 0;
+				break;
+			   case 4: // , found
+				state = 1;
+				break;
+			   case 5: // ; found
+				state = 0;
+				break;
+
+			}
+		}
+	}
+	return 1;
+}
+
+static int config_read(struct defcon_context *ctx TSRMLS_DC)
+{
+	FILE *fd;
+	struct stat st;
+	int res;
+
+	if (0 > stat(ctx->file, &st) || !(fd = VCWD_FOPEN(ctx->file, "r"))) {
+		PR_ERR(ctx, "Cannot open for reading");
+		return 0;
+	}
+
+	if (!st.st_size) {
+		PR_WARN(ctx, "file is empty");
+		return 0;
+	}
+
+	char *str = emalloc(st.st_size + 1);
+
+	size_t slen = fread(str, 1, st.st_size, fd);
+	if (slen < 1) {
+		efree(str);
+		PR_WARN(ctx, "file is empty");
+		return 0;
+	}
+	str[slen] = '\0';
+	fclose(fd);
+
+	res = config_parse(ctx, str TSRMLS_CC);
+
+	efree(str);
+
+	if (!res)
+		PR_DBG(ctx, "PARSING ERROR\n");
+	return res;
+}
+
 PHP_MINIT_FUNCTION(defcon) {
 	REGISTER_INI_ENTRIES();
 
-	FILE *fd;
-	char *file = INI_STR("defcon.config-file");
-	char typ[TYPELEN + 1], N[NAMELEN + 1], V[VALUELEN + 1];
-	int i, j, line = 1;
-	char c;
-	short T, state = 0;
-	struct stat st;
+	struct defcon_context ctx[1];
+	ctx->module_number = module_number;
+	ctx->file = INI_STR("defcon.config-file");
+	ctx->line = 1;
 
-	if(NULL == file || 0 == strcmp(file, "")) {
+	if(NULL == ctx->file || 0 == strcmp(ctx->file, "")) {
 		php_error(E_WARNING, "defcon: No Configfile set...");
 		return SUCCESS;
 	}
 
-	if(-1 != stat(file, &st) && NULL != (fd = VCWD_FOPEN(file, "r"))) {
-
-		if(!st.st_size) {
-			php_error(E_WARNING, "defcon: Configfile empty");
-			return SUCCESS;
-		}
-
-		char *s, *str = emalloc(st.st_size + 1);
-		s = str;
-
-		for(i=0, c=0; c != EOF; i++) {
-			c = fgetc(fd);
-			s[i] = c;
-		}
-		s[i-2] = '\0';
-		fclose(fd);
-
-
-		for(; *s; s++) {
-
-			for(; *s && (*s == ' ' || *s == '\n' || *s == '\t' || *s == '\r'); s++) {
-				if(*s == '\n') line++;
-			}
-	
-			if(!*s)
-			   break;
-
-			if(*s == '#') {
-				for(; *s && *s != '\n'; s++);
-			} else {
-	
-				if(*s == ',')
-				  state = 4;
-				else if(*s == ';')
-				  state = 5;
-
-				switch(state) {
-				   case 0: // catch type
-					for(i = 0; *s && (*s >= 'a' && *s <= 'z' || *s >= 'A' && *s <= 'Z'); s++, i++) {
-						if(i > TYPELEN) {
-							php_error(E_ERROR, "defcon: Typ too long on line %i", line);
-							efree(str);
-							return SUCCESS;
-						}
-						typ[i] = *s;
-					}
-					typ[i] = '\0';
-					T = match_type(typ TSRMLS_DC);
-					if(-1 == T) {
-						php_error(E_ERROR, "defcon: No valid Typ given on line %i (%s)", line, typ);
-						efree(str);
-						return SUCCESS;
-					}
-					state = 1;
-					break;
-				   case 1: // catch varname
-					for(i = 0; *s >= 'a' && *s <= 'z' || *s >= 'A' && *s <= 'Z' || *s >= '0' && *s <= '9' || *s == '_'; s++, i++) {
-						if(i > NAMELEN) {
-							php_error(E_ERROR, "defcon: Varname too long on line %i", line);
-							efree(str);
-							return SUCCESS;
-						}
-						N[i] = *s;
-					}
-					N[i] = '\0';
-					if(N[0] == '\0') {
-						php_error(E_ERROR, "defcon: No Varname set on line %i", line);
-						efree(str);
-						return SUCCESS;
-					}
-
-					if(-1 != match_type(V TSRMLS_DC)) {
-						php_error(E_ERROR, "defcon: Varname should not be a type on line %i", line);
-						efree(str);
-						return SUCCESS;
-					}
-
-					state = 2;
-					s--;
-					break;
-					case 2: // catch =
-					if(*s != '=') {
-						php_error(E_ERROR, "defcon: Strange input on line %i ( = required)", *s, line);
-						efree(str);
-						return SUCCESS;
-					}
-					state = 3;
-					break;
-				   case 3: // final state
-					if(*s == '"' || *s == '\'') c = *(s++);
-					else c = '\0';;
-	
-					for(i=0; *s && (c && *s != c || (!c && (*s >= '0' && *s<='9' || *s >= 'a' && *s<='z' || *s >= 'A' && *s<='Z' || *s == '.'))); s++, i++) {
-						if(i > VALUELEN) {
-							php_error(E_ERROR, "defcon: Value too long on line %i", line);
-							efree(str);
-							return SUCCESS;
-						}
-						V[i] = *s;
-					}
-
-					V[i] = '\0';
-
-					if(c) s++;
-					else if(V[0] == '\0') {
-						php_error(E_ERROR, "defcon: No Value found on line %i", line);
-						efree(str);
-						return SUCCESS;
-	
-					}
-	
-					zend_constant zc;
-	
-					switch(T) {
-					   case 0:
-						zc.value.type = IS_STRING;
-						zc.value.value.str.val = zend_strndup(V, strlen(V));
-						zc.value.value.str.len = strlen(V);
-						break;
-					   case 1:
-					   case 2:
-					   case 9:
-						zc.value.type = IS_LONG;
-						zc.value.value.lval = atol(V);
-						s--;
-						break;
-					   case 3:
-					   case 4:
-					   case 5:
-						zc.value.type = IS_DOUBLE;
-						zc.value.value.dval = atof(V);
-						s--;
-						break;
-					   case 6:
-					   case 7:
-					   case 8:
-						zc.value.type = IS_BOOL;
-						if(0 == strcasecmp(V, "true")) {
-							zc.value.value.lval = 1;
-						} else if(0 == strcasecmp(V, "false")) {
-							zc.value.value.lval = 0;
-						} else {
-							zc.value.value.lval = atol(V);
-						}
-						s--;
-						break;
-					}
-					zc.flags = CONST_CS | CONST_PERSISTENT;
-					zc.name = zend_strndup(N, strlen(N));
-					zc.name_len = strlen(N)+1;
-					zc.module_number = module_number;
-
-					if(zend_register_constant(&zc TSRMLS_CC) == FAILURE) {
-						php_error(E_ERROR, "defcon: Constant '%s' redefined on line %i", N, line);
-					}
-					state = 0;
-					break;
-				   case 4: // , found
-					state = 1;
-					break;
-				   case 5: // ; found
-					state = 0;
-					break;
-	
-				}
-			}
-		}
-
-		efree(str);
-
-	} else {
-		php_error(E_ERROR, "defcon: Can't stat file %s", file);
-		return SUCCESS;
-	}
-
+	config_read(ctx TSRMLS_CC);
 
 	return SUCCESS;
 }
